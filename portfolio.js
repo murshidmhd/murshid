a0db4218962c8b5f5773c8181ba2215cddb676bb
murshidmhd <murshidmuhammad65@gmail.com>
2025-07-03 09:53:51 +0530

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -8,27 +8,29 @@ function sendMail() {
   const serviceID = "service_96ntbbu";
 const templateId = "template_eypswkz";
 
-// emailjs
-//   .send(serviceID, templateId, paramas)
-
-//   .then((res) => {
-//     document.getElementById("name").value = "";
-//     document.getElementById("email").value = "";
-
-//     document.getElementById("message").value = "";
-//     console.log(res);
-//     alert("your message sent successfully");
-//   });
-// .catch((err) => console.log(err));
-// }
-
 emailjs
   .send(serviceID, templateId, paramas)
+
   .then((res) => {
     document.getElementById("name").value = "";
     document.getElementById("email").value = "";
+
     document.getElementById("message").value = "";
-    console.log(res);
-    alert("Your message sent successfully");
+    console.log(res)
+    alert("📨 Message sent! I'll get back to you soon.");
   })
-  .catch((err) => console.log(err));}
+.catch((err) => console.log(err));
+}
+
+// emailjs
+//   .send(serviceID, templateId, paramas)
+//   .then((res) => {
+//     document.getElementById("name").value = "";
+//     document.getElementById("email").value = "";
+//     document.getElementById("message").value = "";
+//     console.log(res);
+//     alert("Your message sent successfully");
+//   })
+//   .catch((err) => console.log(err))
+
+// }
